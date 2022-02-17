b27e7a8efe2c6aafc470c1106c195c5efa746644
Tomytn <Elvistrainer210@gmail.com>
2022-02-17 12:03:21 +0800

    webpack, webpack-cli --save-dev suulgaad file textvvd nemew

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+// module.exports нь яг яаж бидний заасныг хийж ажиллах файлыг bundle Хийхийг зааж өгөхийн тулд хэрэглэнэ
+module.exports = {
+  //bundle хийх гэж байгаа файлын замыг зааж өгнө.
+  entry: "./app/assets/scripts/app.js",
+};
