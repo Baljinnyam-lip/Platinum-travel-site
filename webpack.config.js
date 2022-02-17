a364849c7ccbbb152a1c92917b53798c5aa320c5
Tomytn <Elvistrainer210@gmail.com>
2022-02-17 12:17:16 +0800

    Webpack Өөрчлөлтүүд

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+const path = require("path");
 // module.exports нь яг яаж бидний заасныг хийж ажиллах файлыг bundle Хийхийг зааж өгөхийн тулд хэрэглэнэ
 module.exports = {
   //bundle хийх гэж байгаа файлын замыг зааж өгнө.
   entry: "./app/assets/scripts/app.js",
+  //dist folder дотор байлгамааргүй бол замыг нь өөрчлөх
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  // Шар алдаа зааж буй бичигийг арилгах
+  mode: "development",
+  watch: true,
 };
